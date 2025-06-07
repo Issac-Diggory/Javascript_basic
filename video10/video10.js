dcc33f8e0eb7784ebb7b966bc23c118c53cc05b1
Issac-Diggory <huyfaker016@gmail.com>
2025-06-07 11:18:18 +0700

    video 10,11

--- video10/video10.js
+++ video10/video10.js
@@ -1,1 +1,16 @@
 console.log( 'Học IT với Eric');
+
+let a ='100';
+let b =100;
+console.log(' compare a vs b. a =',a, 'and b =', b, 'result: ', a===b);
+
+let c =[];
+console.log('check value :' ,c);
+let testnull = null;
+console.log('type of testnull:', testnull);
+let d ={};
+console.log('check value :' ,d);
+let e ;
+console.log('check value :' ,e);
+let f ='';
+console.log('check value :' ,f);
---
+++ video11/video11.js
@@ -0,0 +1,8 @@
+console.log(' Học về For loods');
+// for( i = 0; i <= 10; i++){
+//     console.log('check variable i:' ,i);
+// };
+let arr = ['Mu', 'Mancity', 'Liver', 'Chel'];
+for( let i = 0; i < arr.length; i++){
+    console.log('Variable i = ', arr[i]);
+};
