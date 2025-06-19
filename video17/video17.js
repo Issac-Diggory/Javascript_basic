47b020c6f1cb85ee8d088295ad99c7a4b378b76e
Issac-Diggory <huyfaker016@gmail.com>
2025-06-19 14:19:01 +0700

    video17 return

---
+++ video17/video17.js
@@ -0,0 +1,14 @@
+console.log('Học IT nào!');
+//// Return
+
+function sum(a,b,i){
+    console.log('1');
+    console.log('2');
+    console.log('3');
+    console.log('4');
+    if(i===10) return;
+    console.log('5');
+    console.log('6');
+    return a+b;
+};
+console.log(' check sum', sum(9,6,8));
