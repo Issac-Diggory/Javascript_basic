0743b0008cb76449f3cc5bc3023fcc6fd0e770a4
Issac-Diggory <huyfaker016@gmail.com>
2025-06-20 13:34:24 +0700

    Arrow function

---
+++ video18/video18.js
@@ -0,0 +1,16 @@
+console.log('Hello word from HTML');
+console.log('Arrow function');
+console.log('\n');
+console.log(`let name = () => {}`);
+//----Khai báo function truyền thống
+// function sum(a,b) {
+//     return a + b;
+// }
+// let c = sum(1.7,1.5);
+// console.log('Sum c =', c)
+//------------------------
+console.log('Ví dụ');
+let sum =(a,b)=>{
+    return a+b;
+};
+console.log('check sum ', sum(8,9));
