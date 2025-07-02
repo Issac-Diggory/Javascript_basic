6742fb50e51882b2715c005a3de8f6037c9bbd1f
Issac-Diggory <huyfaker016@gmail.com>
2025-07-02 22:58:40 +0700

    map

---
+++ video22/video22.js
@@ -0,0 +1,16 @@
+console.log(' hello word from HTML');
+
+let arr= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (let i = 0; i < arr.length; i++){
+    arr[i] = arr[i] * arr[i]
+}
+
+// let mapArr= arr.map((item, index) => {
+//     item = item * item
+//     return item;  
+// })
+
+
+console.log(' Check value i', arr);
+// console.log(' Check value i', mapArr);
+
